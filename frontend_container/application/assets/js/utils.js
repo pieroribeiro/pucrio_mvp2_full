2008192f5c60d32117487a0f17818001b8c72815
Piero Ribeiro <piero.webmaster@gmail.com>
2024-04-02 20:56:13 -0300

    frontend: ajustes finais para exclusão de apis com notificação

--- frontend_container/application/assets/js/utils.js
+++ frontend_container/application/assets/js/utils.js
@@ -1,21 +1,7 @@
 function showAlert(message, type) {
-    $('#alertContainer').prepend(`<div class="alert alert-${type} alert-dismissible fade show" role="alert"><strong>Atenção!</strong> <span class="message">${message}</span><button type="button" style="float: right;" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`)
+    $('#alertContainer').prepend(`<div class="alert alert-${type} fade show" role="alert"><strong>Atenção!</strong> <span class="message">${message}</span></div>`)
 
-    $('.alert button').on('click', () => {
+    setTimeout(() => {
         $(".alert").alert('close')
-    })
-    
-
-
-    // alert.close()
-    // const alertElement = $('#alert')
-    // alertElement.children("#message").addClass(`alert-${type}`).append(message)
-    // alertElement.alert()
-
-    // setTimeout(() => {
-    //     $("#alert #message").empty().removeClass().addClass("alert")
-    //     alertElement.alert("close")
-    // }, 5000)
+    }, 5000)
 }
-
-showAlert('Testando mensagem', 'warning')
