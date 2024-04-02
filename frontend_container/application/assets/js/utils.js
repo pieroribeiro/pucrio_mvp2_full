2933f36c53bc8a02a07fc9cd5798f6419c542df2
pieroribeirosbt <pieroribeiro@sbt.com.br>
2024-04-02 18:15:10 -0300

    frontend: ajsutando mensagens no html

--- frontend_container/application/assets/js/utils.js
+++ frontend_container/application/assets/js/utils.js
@@ -1,10 +1,21 @@
 function showAlert(message, type) {
-    const alertElement = $('#alert')
-    alertElement.children("#message").addClass(`alert-${type}`).append(message)
-    alertElement.alert()
+    $('#alertContainer').prepend(`<div class="alert alert-${type} alert-dismissible fade show" role="alert"><strong>Atenção!</strong> <span class="message">${message}</span><button type="button" style="float: right;" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`)
 
-    setTimeout(() => {
-        $("#alert #message").empty().removeClass().addClass("alert")
-        alertElement.alert("close")
-    }, 5000)
+    $('.alert button').on('click', () => {
+        $(".alert").alert('close')
+    })
+    
+
+
+    // alert.close()
+    // const alertElement = $('#alert')
+    // alertElement.children("#message").addClass(`alert-${type}`).append(message)
+    // alertElement.alert()
+
+    // setTimeout(() => {
+    //     $("#alert #message").empty().removeClass().addClass("alert")
+    //     alertElement.alert("close")
+    // }, 5000)
 }
+
+showAlert('Testando mensagem', 'warning')
